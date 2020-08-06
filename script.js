78edb71368c0980c14dca3898be4db7230813305
Mohamed Irfan <irfancuber@gmail.com>
2020-08-06 14:57:57 +0800

    Added a few more functions

--- script.js
+++ script.js
@@ -101,7 +101,8 @@ var blue = 0;
 var rotationOffset = -90;
 var colorAddition = 25.5;
 var rotationAddition = 9;
-var countdown = 4;
+var originalCountdown = 5;
+var countdown = originalCountdown;
 var quaters = document.querySelectorAll(".quaters");
 var score = 0;
 var highscore = 0;
@@ -116,12 +117,34 @@ function displayWordOnPanel(final) {
     currentScrollWord.style.color = "rgb(160, 57, 87)";
 }
 
-function bouncyIntro() {
+function addBouncyIntro() {
     // document.getElementsByClassName("bouncy")[0].classList.add("bouncyIntro");
     document.querySelector("#q1").classList.add("q1c");
     document.querySelector("#q2").classList.add("q2c");
     document.querySelector("#q3").classList.add("q3c");
     document.querySelector("#q3").classList.add("q4c");
+}
+
+function removeBouncyIntro() {
+    document.getElementsByClassName("bouncy")[0].classList.remove("bouncyIntro");
+    document.getElementsByClassName("bouncy")[0].style.opacity = "1";
+    document.querySelector("#q1").classList.remove("q1c");
+    document.querySelector("#q2").classList.remove("q2c");
+    document.querySelector("#q3").classList.remove("q3c");
+    document.querySelector("#q3").classList.remove("q4c"); 
+    document.getElementsByClassName("bouncy")[0].classList.remove("bounce");  
+}
+
+// When the apps is first started or upon refresh
+window.onload = function() {
+    var final = "";
+    for(var i = 0; i < words[level].length; i++){
+        final += `<li>${words[level][i]}</li>`;
+    }
+    this.addBouncyIntro();
+    this.displayWordOnPanel(final);
+    // Display the word to type
+    document.querySelector("#word").innerHTML = currentWord;
 }
 
 function movers(index) {
@@ -131,20 +154,135 @@ function movers(index) {
     }
 }
 
+// Function to fill circle with colors as game progresses
+function setColors() {
+    if (green === 255 && red < 255 && blue ===0){
+        this.movers(1);
+    }
+    else if (green > 0 && red === 255 && blue ===0){
+        this.movers(2);
+    }
+    else if (green === 0 && red === 255 && blue < 255){
+        this.movers(3);
+    }
+}
 
+// Function to reset the circle color when changing level
+function resetCirlce(){
+    red = blue = 0;
+    green = 255;
+    rotationOffset = -90;
+    quaters.forEach(quater => {
+        var newColor = `rgb(${red.toString()}, ${green.toString()}, ${blue.toString()})`;
+        quater.style.borderLeftColor = newColor;
+        quarter.style.borderTopColor = newColor;
+        quarter.style.transform = `rotate(${rotationOffset.toString()}deg)`;
+    })
+}
 
+//TO CHANGE!
 function changeLevel(newLevel) {
     input.value = "";
-    this.bouncyIntro();
-}
-// When the apps is first started or upon refresh
-window.onload = function() {
+    this.addBouncyIntro();
+    // Set new level and change word list for use
+    level = newLevel;
+    pointer = 0;
+    currentWord = words[level][pointer];
     var final = "";
-    for(var i = 0; i < words[level].length; i++){
-        final += `<li>${words[level][i]}</li>`;
+    for(var i = 0; i < words[level].length; i++) {
+        final += "<li>" + words[level][i] + "</li>";
     }
-    this.bouncyIntro();
-    this.displayWordOnPanel(final);
-    // Display the word to type
-    document.querySelector("#word").innerHTML = currentWord;
+    document.getElementsByTagName("ul")[0].innerHTML = final;
+    document.getElementById("word").innerHTML = currentWord;
+    offset = originalOffset;
+    var currentScroll = document.getElementsByTagName("li")[pointer];
+    currentScroll.style.fontSize = "19pt";
+    currentScroll.style.fontWeight = "bold";
+    currentScroll.style.color = "rgb(160, 57, 87)";
+    var setLevel = level===2?"Coding":level;
+    document.getElementById("message").innerHTML = "Level-" + level.toString();
+    box.style.marginTop = offset.toString() + "px";
+}
+
+function setScore() {
+    score += countdown;
+    document.querySelector("#score").innerHTML = score.toString();
+}
+
+function setHighScore() {
+    if (score > highscore){
+        highscore = score;
+        document.querySelector("#highscore").innerHTML = highscore.toString();
+    }
+    // Reset current score
+    score = 0;
+    document.querySelector("#score").innerHTML = score.toString();
+}
+
+function decodeHtml(html) {
+    var textArea = document.createElement("textarea");
+    textArea.innerHTML = html;
+	return textArea.value;
+}
+
+input.oninput = function() {
+    if (level === 0 && pointer === 0 && input.value.length === 1){
+        var timed = setInterval(clockHandler, 1000);
+        function clockHandler(timed) {
+            if (countdown > 0){
+                countdown--;
+                if (countdown === 0){
+                    document.querySelector("#message").innerHTML = "Game Over!";
+                    document.querySelector("#message").style.color = "rgb(255, 0, 0)";
+                    input.blur();
+                    input.value = "";
+                    setTimeout(() => {
+                        clearInterval(timed);
+                        changeLevel(0);
+                        countdown = originalCountdown;
+                        setHighScore();
+                        document.querySelector("#seconds").innerHTML = originalCountdown;
+                    }, 1000);
+                }
+            }
+            document.querySelector("#seconds").innerHTML = countdown;
+        }
+    }
+    removeBouncyIntro();
+
+    if (input.value === decodeHtml(words[level][pointer])) {
+        setColors();
+        setScore();
+        if (level === 0 && pointer >=19 && pointer != 29)
+            countdown = 3;
+        else
+            countdown = 4;
+        document.getElementById("secs").innerHTML = countdown;
+        if (blue === 255) {
+            var n = level + 1;
+            changeLevel(n);
+        }
+        else {
+            pointer += 1;
+            currentWord = words[level][pointer];
+            document.getElementById("word").innerHTML = currentWord;
+            var currentScroll = document.getElementsByTagName("li")[pointer-1];
+            currentScroll.style.fontSize = "13pt";
+            currentScroll.style.fontWeight = "";
+            currentScroll.style.color = "rgb(208, 147, 129)";
+            currentScroll = document.getElementsByTagName("li")[pointer];
+            currentScroll.style.fontSize = "19pt";
+            currentScroll.style.fontWeight = "bold";
+            currentScroll.style.color = "rgb(160, 57, 87)";
+            document.getElementsByClassName("bouncy")[0].classList.add("bounce");
+            offset = offset - nextOffset;
+            quaters.forEach(quater => {
+                var c = "rgb(" + red.toString() + "," + green.toString() + "," + blue.toString() + ")";
+                quater.style.borderLeftColor = c;
+                quater.style.borderTopColor = c;
+            })
+            box.style.marginTop = offset.toString() + "px";
+            input.value = "";
+        }   
+    }
 }
